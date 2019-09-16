71dd3f30ce6a73a8f0d95842ebbe3e5207fe65c9
Uzair Tariq <uzair0213@icloud.com>
2019-09-16 13:44:18 -0600

    Add cars to inventory

--- Car-Dealership/src/Main.java
+++ Car-Dealership/src/Main.java
@@ -1,13 +1,54 @@
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        Car BMWSomething = new Car(
+        Car carOne = new Car(
                 "BMW",
-                "Something",
-                1998,
-                135450,
-                "green",
+                "X3",
+                2013,
+                58769,
+                "blue",
                 15590
         );
-        System.out.println(BMWSomething.getColour());
+        Car carTwo = new Car(
+                "Honda",
+                "Civic",
+                2009,
+                98047,
+                "white",
+                9590
+        );
+        Car carThree = new Car(
+                "Toyota",
+                "Corolla",
+                2004,
+                103578,
+                "black",
+                3498
+        );
+        Car carFour = new Car(
+                "Kia",
+                "Sorento",
+                2007,
+                87908,
+                "silver",
+                7631
+        );
+        Car carFive = new Car(
+                "Honda",
+                "Accord",
+                2013,
+                56347,
+                "black",
+                13485
+        );
+        Car carSix = new Car(
+                "Mercedes",
+                "GLC",
+                2010,
+                48781,
+                "white",
+                18970
+        );
     }
 }
