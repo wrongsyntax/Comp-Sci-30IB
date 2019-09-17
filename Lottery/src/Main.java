82fd94bbbc616c370d8d7c26f2df762a11badfc0
Uzair Tariq <uzair0213@icloud.com>
2019-09-17 13:54:38 -0600

    Create method to draw numbers
    Still need to create a way to remove numbers drawn twice.

--- Lottery/src/Main.java
+++ Lottery/src/Main.java
@@ -1,2 +1,12 @@
+import java.util.Arrays;
+
 public class Main {
+    public static void main(String[] args) {
+        // Instantiation of Numbers object
+        Numbers lottery = new Numbers(49);
+
+        // Call method to draw numbers
+        int[] drawnNums = lottery.draw();
+        System.out.println("The drawn numbers are " + Arrays.toString(drawnNums) + ".");
+    }
 }
--- Lottery/src/Numbers.java
+++ Lottery/src/Numbers.java
@@ -1,2 +1,22 @@
+import java.util.Random;
+
 public class Numbers {
+    // Instance variables
+    private Random random;
+    private int maxNum;
+
+    // Constructor
+    public Numbers(int maxNum) {
+        this.maxNum = maxNum;
+        random = new Random();
+    }
+
+    public int[] draw() {
+        int[] drawnNums = {0, 0, 0, 0, 0, 0};
+        for (int i = 0; i < 6; i++) {
+            int draw = random.nextInt(maxNum) + 1;
+            drawnNums[i] = draw;
+        }
+        return drawnNums;
+    }
 }
