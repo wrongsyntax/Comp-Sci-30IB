9ce832b164680f9dd13173b960d6f3e2989c7ca7
Uzair Tariq <uzair0213@icloud.com>
2019-10-07 13:31:54 -0600

    Use for loop to roll three times && small formatting change

--- Yahtzee/src/Main.java
+++ Yahtzee/src/Main.java
@@ -5,20 +5,22 @@ public class Main {
         // Instantiation of Dice object
         Dice rollOne = new Dice(6);
 
-        // Call method to roll five dice
-        int[] rolledNums = rollOne.roll();
-        System.out.println("You rolled " + Arrays.toString(rolledNums));
+        for (int i = 0; i < 3; i ++) {
+            // Call method to roll five dice
+            int[] rolledNums = rollOne.roll();
+            System.out.println("You rolled " + Arrays.toString(rolledNums));
 
-        // Instantiation of CheckAllEqual object
-        CheckAllEqual check = new CheckAllEqual(rolledNums);
+            // Instantiation of CheckAllEqual object
+            CheckAllEqual check = new CheckAllEqual(rolledNums);
 
-        // Check to see if a Yahtzee was rolled
-        boolean yahtzeeRolled = check.check();
+            // Check to see if a Yahtzee was rolled
+            boolean yahtzeeRolled = check.check();
 
-        if (yahtzeeRolled) {
-            System.out.println("That's a Yahtzee!");
-        } else {
-            System.out.println("That is not a Yahtzee.");
+            if (yahtzeeRolled) {
+                System.out.println("That's a Yahtzee! \n");
+            } else {
+                System.out.println("That is not a Yahtzee. \n");
+            }
         }
     }
 }
