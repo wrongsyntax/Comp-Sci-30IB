93b135f0519834c4cbfddf394f034280d9ab589b
Uzair Tariq <uzair0213@icloud.com>
2019-10-31 10:54:48 -0600

    Restart due to overcomplicated code in previous version

--- Bowling/src/Game.java
+++ Bowling/src/Game.java
@@ -1,34 +1,3 @@
-import java.util.Scanner;
-
-class Game {
-    // Instance Variables
-    private int frames;
-    private int throwsLeft;
-    private int points;
-    private int randChosenInt;
-    private Pins pins;
-    private Scanner scanner;
-
-    // Constructor
-    Game(Pins pins, int randChosenInt) {
-        this.pins = pins;
-        this.randChosenInt = randChosenInt;
-        frames = 10;
-        throwsLeft = 2;
-        points = 0;
-        scanner = new Scanner(System.in);
-    }
-
-    // Method to ask for a number
-    // This is to see how close the number is to the randomly generated one and award points accordingly
-    int askForNum() {
-        System.out.println("Guess a number between 1 and 10: ");
-        return scanner.nextInt();
-    }
-
-    // Method to knock down the pins
-    // If the numbers are the same, it's a strike
-    int awardPoints(int guess) {
-        return points;
-    }
+public class Game {
+    
 }
