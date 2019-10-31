1cec82094ae337e74541d9e6b51d8b1bb594c213
Uzair Tariq <uzair0213@icloud.com>
2019-10-31 10:42:57 -0600

    Add more stuff to try and get this to work somewhat

--- Bowling/src/Game.java
+++ Bowling/src/Game.java
@@ -1,5 +1,34 @@
-public class Game {
+import java.util.Scanner;
+
+class Game {
     // Instance Variables
-    int frames = 10;
-    int throwsLeft = 2;
+    private int frames;
+    private int throwsLeft;
+    private int points;
+    private int randChosenInt;
+    private Pins pins;
+    private Scanner scanner;
+
+    // Constructor
+    Game(Pins pins, int randChosenInt) {
+        this.pins = pins;
+        this.randChosenInt = randChosenInt;
+        frames = 10;
+        throwsLeft = 2;
+        points = 0;
+        scanner = new Scanner(System.in);
+    }
+
+    // Method to ask for a number
+    // This is to see how close the number is to the randomly generated one and award points accordingly
+    int askForNum() {
+        System.out.println("Guess a number between 1 and 10: ");
+        return scanner.nextInt();
+    }
+
+    // Method to knock down the pins
+    // If the numbers are the same, it's a strike
+    int awardPoints(int guess) {
+        return points;
+    }
 }
--- Bowling/src/Pins.java
+++ Bowling/src/Pins.java
@@ -1,21 +1,11 @@
 import java.util.Random;
 
-public class Pins {
+class Pins {
     // Instance Variables
-    private int numOfPins;
-    private int strikePoints;
-    private int sparePoints;
-    private int pointsPerPin;
-    private int points;
     private Random random;
 
     // Constructor
-    public Pins() {
-        numOfPins = 10;
-        strikePoints = 20;
-        sparePoints = 15;
-        pointsPerPin = 1;
-        points = 0;
+    Pins() {
         random = new Random();
     }
 
@@ -23,12 +13,5 @@ public class Pins {
     // Using the Throw class, this is used to award points according to how close the player's guess was to this number
     int chooseRandInt() {
         return random.nextInt(10) + 1;
-    }
-
-    // Method to knock down the pins
-    // If the numbers are the same, it's a strike
-    int pointsAwarded(int randInt, int guess) {
-        // TODO: Figure out a way to award a different number of points according to how close the guess is
-        return points;
     }
 }
