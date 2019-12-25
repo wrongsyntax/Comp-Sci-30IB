7df93201436ae2e5a2daa1ba5b9e2c1c36a5b183
Uzair Tariq <uzair0213@icloud.com>
2019-12-24 19:42:31 -0700

    A bit of code to deal opponent's cards

--- Go-Fish/src/Main.java
+++ Go-Fish/src/Main.java
@@ -4,11 +4,15 @@ public class Main {
     public static void main(String[] args) {
         Deck deck = new Deck();
         Player player = new Player();
+        Player opponent = new Player();
 
         String[] fullDeck = deck.createDeck();
         System.out.println(Arrays.toString(fullDeck));
 
         String[] currentHand = player.getDealtCards(fullDeck);
         System.out.println("You've been dealt the following cards: " + Arrays.toString(currentHand));
+
+        String[] opponentHand = opponent.getDealtCards(fullDeck);
+        System.out.println("Leaked info - opponent's hand: " + Arrays.toString(opponentHand));
     }
 }
