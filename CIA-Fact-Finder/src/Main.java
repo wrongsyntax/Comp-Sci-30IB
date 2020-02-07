02d30444b0bad13622fd7942cb096ecb724e4fd8
Uzair Tariq <uzair0213@icloud.com>
2020-02-06 21:49:01 -0700

    Finished assignment

--- CIA-Fact-Finder/src/Main.java
+++ CIA-Fact-Finder/src/Main.java
@@ -2,8 +2,32 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        // Instance variables
         CSVReader csvReader = new CSVReader();
         csvReader.parseFile();
         String[][] fullList = csvReader.fullList;
+        Retriever infoRetriever = new Retriever(fullList);
+
+        int countryIndex = infoRetriever.askForCountry();
+        System.out.println("User chose: " + fullList[countryIndex][0] + "\n");
+
+        int dataIndex = infoRetriever.askForData();
+        if (dataIndex != 5) {
+            System.out.println("User asked for: " + fullList[0][dataIndex].replace("_", " ").toLowerCase());
+        } else {
+            System.out.println("User asked for: " + fullList[0][dataIndex].replace("_", " "));
+        }
+
+        if (dataIndex == 1) {
+            System.out.println("The rank of " + fullList[countryIndex][0] + " is " + fullList[countryIndex][dataIndex]);
+        } else if (dataIndex == 2) {
+            System.out.println("The birth rate of " + fullList[countryIndex][0] + " is " + fullList[countryIndex][dataIndex]);
+        } else if (dataIndex == 3) {
+            System.out.println("The population of " + fullList[countryIndex][0] + " is " + fullList[countryIndex][dataIndex] + " million people");
+        } else if (dataIndex == 4) {
+            System.out.println("The unemployment rate of " + fullList[countryIndex][0] + " is " + fullList[countryIndex][dataIndex] + "%");
+        } else if (dataIndex == 5) {
+            System.out.println("The GDP of " + fullList[countryIndex][0] + " is $" + fullList[countryIndex][dataIndex] + " billion");
+        }
     }
 }
--- CIA-Fact-Finder/src/Retriever.java
+++ CIA-Fact-Finder/src/Retriever.java
@@ -1,2 +1,44 @@
+import java.util.Scanner;
+
 public class Retriever {
+    // Instance variables
+    String[][] data;
+    Scanner scanner = new Scanner(System.in);
+    int countryIndex;
+    int dataIndex;
+
+    // Constructor
+    public Retriever(String[][] fullList) {
+        data = fullList;
+    }
+
+    // Method to ask for which country the user wants to search
+    int askForCountry() {
+        // Print out all the options the user can choose from
+        for (int i = 1; i < data.length; i++) {
+            if (i < 10) {
+                System.out.println("(" + i + ")" + "  " + data[i][0].replace("_", " "));
+            } else {
+                System.out.println("(" + i + ")" + " " + data[i][0].replace("_", " "));
+            }
+        }
+
+        System.out.println();
+        System.out.println("Enter the number of one of the countries above: ");
+        countryIndex = scanner.nextInt();
+
+        return countryIndex;
+    }
+
+    int askForData() {
+        for (int i = 1; i < data[0].length; i++) {
+            System.out.println("(" + i + ")" + " " + data[0][i].replace("_", " "));
+        }
+
+        System.out.println();
+        System.out.println("Choose the data you would like from the list above: ");
+        dataIndex = scanner.nextInt();
+
+        return dataIndex;
+    }
 }
