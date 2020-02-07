9aca661ebb8a8750f1475bdd478002f54d7bb9b5
Uzair Tariq <uzair0213@icloud.com>
2020-02-06 21:06:02 -0700

    File is now able to be parsed and data is stored in a 2D array

--- CIA-Fact-Finder/src/CSVReader.java
+++ CIA-Fact-Finder/src/CSVReader.java
@@ -1,2 +1,27 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class CSVReader {
+    // Instance variables
+    BufferedReader reader = new BufferedReader(new FileReader("ciaFactFinder.csv"));
+    String line = "";
+    String[][] fullList = new String[26][];
+
+    // Constructor
+    public CSVReader() throws FileNotFoundException {
+    }
+
+    // Method to parse the csv file and compile all the data into a 2D array
+    void parseFile() throws IOException {
+        line = reader.readLine();
+        int i = 0;
+        while (line != null) {
+            String[] country = line.split(",");
+            fullList[i] = country;
+            i++;
+            line = reader.readLine();
+        }
+    }
 }
--- CIA-Fact-Finder/src/Main.java
+++ CIA-Fact-Finder/src/Main.java
@@ -1,5 +1,9 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.IOException;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        CSVReader csvReader = new CSVReader();
+        csvReader.parseFile();
+        String[][] fullList = csvReader.fullList;
     }
 }
